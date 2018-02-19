f0119c368f871ab75bfdad27d6a005284c940593
Maria Tsvetkova <tsvetkovamariia@gmail.com>
2018-02-19 16:38:07 +0600

    add mobile menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
 $(document).ready(function(){
+  // Main screen slider
   $('.presentation').slick({
     autoplay: true,
     arrows: false,
     dots: true,
     dotsClass: 'dots',
     mobileFirst: true    
+  });
+
+  // mobile menu
+  // make sure mobile menu is hidden on first appearance on mobile screens
+  var itaLanding = {
+    mobileMenu: $('.menu'),
+    toggleButton: $('.menu-toggle'),
+    mobileMenuBreakpoint: 900
+  }
+  itaLanding.hideMenu = function(e){
+    itaLanding.mobileMenu.hide();
+  }
+  itaLanding.toggleMenu = function(e){
+    itaLanding.mobileMenu.slideToggle(300);
+  }
+  itaLanding.showMenu = function(e) {
+    itaLanding.mobileMenu.show();
+  }
+  itaLanding.toggleMenuOnWindowResize = function(e){
+    if(window.innerWidth > itaLanding.mobileMenuBreakpoint) {
+      itaLanding.showMenu();
+      itaLanding.toggleButton.addClass('open');
+    } else {
+      itaLanding.hideMenu();
+      itaLanding.toggleButton.removeClass('open');
+    }
+  }
+  
+  itaLanding.toggleMenuOnWindowResize();
+
+  itaLanding.toggleButton.click(function(e) {
+    e.preventDefault();
+    itaLanding.toggleButton.toggleClass('open');
+    itaLanding.toggleMenu();
+  });
+
+  $(window).resize(function(e){
+    itaLanding.toggleMenuOnWindowResize();
   });
 });
